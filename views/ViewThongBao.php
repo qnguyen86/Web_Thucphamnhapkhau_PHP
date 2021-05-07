577ae92790c1232baa3607c03c6249ed56996fcf
qnguyen86 <nhqb612@gmail.com>
2021-05-07 18:30:46 +0700

    Create ViewThongBao.php
    Thêm ViewThongBao cho thông báo thành công thanh toán đơn hàng

---
+++ views/ViewThongBao.php
@@ -0,0 +1,9 @@
+<?php 
+  //load file LayoutTrangChu.php
+  $this->fileLayout = "LayoutTrangTrong.php";
+ ?>
+ <div class="template-cart">
+   <h3>Thanh toán thành công</h3>
+   <h3>Vui lòng vào đơn hàng để xem tình trạng đơn hàng</h3>
+  <h3 style="margin-top: 20px;"><a href="index.php" class="btn btn-success button pull-left">Tiếp tục mua hàng</a></h3>
+</div>
